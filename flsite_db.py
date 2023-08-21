a780d482eb02c71baa6dc8117f2edfc36a926b19
Daniil-Melnik <daniil.melnik.03@bk.ru>
2023-08-21 11:34:08 +0300

    #8 create db

---
+++ flsite_db.py
@@ -0,0 +1,30 @@
+import sqlite3
+import os
+from flask import Flask, render_template, request
+
+# configuration
+DATABASE = '/tmp/flsite.db'
+DEBUG = True
+SECRET_KEY = "qseft1234"
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+app.config.update(dict(DATABASE=os.path.join(app.root_path, 'flsite.db')))
+
+def connect_db():
+  conn = sqlite3.connect(app.config['DATABASE'])
+  conn.row_factory = sqlite3.Row
+  return conn
+
+def create_db():
+  db = connect_db()
+  with app.open_instance_resource('db_sql.sql', mode = 'r') as f:
+    db.cursor().executescript(f.read())
+  db.commit()
+  db.close()
+   
+create_db()
+
+# if __name__ == "__main__":
+#     app.run(debug = True)
