ba72a1f091d0877f573ad9cf4c7ac1f1e132dcaa
Daniil-Melnik <daniil.melnik.03@bk.ru>
2023-08-21 14:20:01 +0300

    #9 added adding post to database from special form

--- flsite_db.py
+++ flsite_db.py
@@ -1,6 +1,6 @@
 import sqlite3
 import os
-from flask import Flask, render_template, request, g
+from flask import Flask, flash, render_template, request, g
 from FDataBase import FDataBase
 
 # configuration
@@ -42,7 +42,22 @@ def close_db(error):
   if hasattr(g, 'link_db'):
     g.link_db.close()
 
-# if __name__ == "__main__":
-#     app.run(debug = True)
+@app.route("/add_post", methods=["POST", "GET"])
+def addPost():
+  db = get_db()
+  dbase = FDataBase(db)
 
-create_db()
+  if request.method == "POST":
+    if len(request.form['name']) > 4 and len(request.form['post']) > 10:
+      res = dbase.addPost(request.form['name'], request.form['post'])
+      if not res:
+        flash('Ошибка добавления статьи', category= 'error')
+      else:
+        flash('Статья добавлена успешно', category='success')
+    else:
+      flash('Ошибка добавления статьи', category='error')
+  return render_template('add_post.html', hesh = dbase.getMenu(), title = "Добавление статьи")
+
+if __name__ == "__main__":
+    app.run(debug = True)
+
