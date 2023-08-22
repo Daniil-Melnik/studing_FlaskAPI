f937f97baa6b38297f2035b48c75750aa1b5373f
Daniil-Melnik <daniil.melnik.03@bk.ru>
2023-08-22 14:04:12 +0300

    #11 add redirecting

--- flsite_resp.py
+++ flsite_resp.py
@@ -1,5 +1,4 @@
-from flask import Flask, make_response, render_template
-import codecs
+from flask import Flask, make_response, redirect, render_template, url_for
 
 app = Flask(__name__)
 
@@ -7,15 +6,15 @@ _hesh = [{"title": "Главная", "url": "/"}, {"title": "Добавить с
 
 @app.route("/index_img")
 def index_img():
-    img = None
-    with app.open_resource( app.root_path + "/static/images_html/framework-flask-intro.files/map.jpg", mode="rb") as f:
-        img = f.read()
-    if img is None:
-        return "None image"
+  img = None
+  with app.open_resource( app.root_path + "/static/images_html/framework-flask-intro.files/map.jpg", mode="rb") as f:
+    img = f.read()
+  if img is None:
+    return "None image"
     
-    res = make_response(img)
-    res.headers['Content-Type'] = 'image/jpg'
-    return res
+  res = make_response(img)
+  res.headers['Content-Type'] = 'image/jpg'
+  return res
 
 @app.route("/index_text")
 def index_text():
@@ -24,6 +23,10 @@ def index_text():
   res.headers['Content-Type'] = 'text/plain'
   res.headers['Server'] = 'flassksite'
   return res
+
+@app.route('/transfer')
+def transfer():
+  return redirect(url_for('index_text'), 301)
 
 if __name__ == "__main__":
     app.run(debug = True)
