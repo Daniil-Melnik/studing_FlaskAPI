507299614da0e7018848085ae3582008ce131cbb
Daniil-Melnik <daniil.melnik.03@bk.ru>
2023-08-22 13:25:23 +0300

    #11 add image response

--- flsite_resp.py
+++ flsite_resp.py
@@ -6,10 +6,14 @@ _hesh = [{"title": "Главная", "url": "/"}, {"title": "Добавить с
 
 @app.route("/")
 def index():
-    content = render_template('index.html', hesh = _hesh, posts = [])
-    res = make_response(content)
-    res.headers['Content-Type'] = 'text/plain'
-    res.headers['Server'] = 'flasksite'
+    img = None
+    with app.open_resource( app.root_path + "/static/images_html/framework-flask-intro.files/map.jpg", mode="rb") as f:
+        img = f.read()
+    if img is None:
+        return "None image"
+    
+    res = make_response(img)
+    res.headers['Content-Type'] = 'image/jpg'
     return res
 
 if __name__ == "__main__":
