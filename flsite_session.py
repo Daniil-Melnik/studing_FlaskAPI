dca770331f1ccbc6a2d83e5637d435c7f2a23c0b
Daniil-Melnik <daniil.melnik.03@bk.ru>
2023-08-22 21:26:58 +0300

    #13 add session with lifetime and data

--- flsite_session.py
+++ flsite_session.py
@@ -1,7 +1,9 @@
+import datetime
 from flask import Flask, render_template, make_response, url_for, request, session
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'qseft1234'
+app.permanent_session_lifetime = datetime.timedelta(days = 10)
 
 @app.route('/')
 def index():
@@ -10,6 +12,17 @@ def index():
   else:
     session['visits'] = 1
   return(f"<h1>Main page</h1><p>Число просмотров: {session['visits']}")
+
+data = [1, 2, 3, 4]
+@app.route("/session")
+def session_data():
+  session.permanent = True
+  if 'data' not in session:
+    session['data'] = data
+  else:
+    session['data'][1] += 1
+    session.modified = True
+  return f"<p>session['data']: {session['data']}"
 
 if __name__ == "__main__":
   app.run(debug = True)
