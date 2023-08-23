c792771507934a0ea0b4566ad2e0ba6568cb50d1
Daniil-Melnik <daniil.melnik.03@bk.ru>
2023-08-23 16:49:36 +0300

    #17 add css to template

--- UserLogin.py
+++ UserLogin.py
@@ -11,3 +11,9 @@ class UserLogin(UserMixin):
     
   def get_id(self):
     return str(self.__user['id'])
+  
+  def getName(self):
+    return self.__user['name'] if self.__user else "Без имени"
+  
+  def getEmail(self):
+    return self.__user['email'] if self.__user else "Без email"
